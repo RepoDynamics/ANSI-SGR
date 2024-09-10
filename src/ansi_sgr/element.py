5bb8095113786236606f2fab1847e258b0a275a2
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-10 08:29:50 +0200

    update

--- src/ansi_sgr/element.py
+++ src/ansi_sgr/element.py
@@ -2,6 +2,7 @@ from __future__ import annotations as _annotations
 
 from typing import TYPE_CHECKING as _TYPE_CHECKING
 import textwrap as _textwrap
+import wcwidth as _wcwidth
 
 import ansi_sgr as _sgr
 
@@ -13,62 +14,71 @@ if _TYPE_CHECKING:
 def admonition(
     title: Stringable,
     text: Stringable,
+    emoji: Stringable | None = None,
     title_styles: int | str | list[int | str] = None,
     title_color: int | str | tuple = None,
     title_bg_color: int | str | tuple = None,
-    title_char_top: Stringable | None = None,
-    title_char_bottom: Stringable | None = None,
-    title_char_left: Stringable | None = None,
-    title_char_right: Stringable | None = None,
+    title_margin_top: int | None = None,
+    title_margin_bottom: int | None = None,
+    title_margin_left: int | None = None,
+    title_margin_right: int | None = None,
     title_align: Literal["left", "right", "center"] = "left",
     text_styles: int | str | list[int | str] = None,
     text_color: int | str | tuple = None,
     text_bg_color: int | str | tuple = None,
-    text_char_top: Stringable | None = None,
-    text_char_bottom: Stringable | None = None,
-    text_char_left: Stringable | None = None,
-    text_char_right: Stringable | None = None,
-    margin_top: int | None = None,
-    margin_middle: int | None = None,
-    margin_bottom: int | None = None,
-    margin_left: int | None = None,
-    margin_right: int | None = None,
-    line_width: int = 50,
+    text_margin_top: int | None = None,
+    text_margin_bottom: int | None = None,
+    text_margin_left: int | None = None,
+    text_margin_right: int | None = None,
+    text_align: Literal["left", "right", "center"] = "left",
+    char_top: Stringable | None = "━",
+    char_bottom: Stringable | None = "━",
+    char_left: Stringable | None = "┃",
+    char_right: Stringable | None = "┃",
+    char_top_left: Stringable | None = "┏",
+    char_top_right: Stringable | None = "┓",
+    char_bottom_left: Stringable | None = "┗",
+    char_bottom_right: Stringable | None = "┛",
+    line_width: int = 70,
 ):
     title = str(title)
-    if title_align == "left":
-        title = title.ljust(line_width)
-    elif title_align == "right":
-        title = title.rjust(line_width)
-    else:
-        title = title.center(line_width)
+    if emoji:
+        title = f"{emoji} {title}"
     title_section = block(
-        title,
+        text=title,
         text_styles=title_styles,
         text_color=title_color,
         bg_color=title_bg_color,
-        char_top=title_char_top,
-        char_bottom=title_char_bottom,
-        char_left=title_char_left,
-        char_right=title_char_right,
-        margin_top=margin_top,
-        margin_left=margin_left,
-        margin_right=margin_right,
+        char_top=char_top,
+        char_bottom="",
+        char_left=char_left,
+        char_right=char_right,
+        char_top_left=char_top_left,
+        char_top_right=char_top_right,
+        margin_top=title_margin_top,
+        margin_bottom=title_margin_bottom,
+        margin_left=title_margin_left or text_margin_left,
+        margin_right=title_margin_right or text_margin_right,
+        line_width=line_width,
+        align=title_align,
     )
     body_section = block(
         text=text,
         text_styles=text_styles,
         text_color=text_color,
         bg_color=text_bg_color,
-        char_top=text_char_top,
-        char_bottom=text_char_bottom,
-        char_left=text_char_left,
-        char_right=text_char_right,
-        margin_top=margin_middle,
-        margin_bottom=margin_bottom,
-        margin_left=margin_left,
-        margin_right=margin_right,
+        char_top="",
+        char_bottom=char_bottom,
+        char_left=char_left,
+        char_right=char_right,
+        char_bottom_left=char_bottom_left,
+        char_bottom_right=char_bottom_right,
+        margin_top=text_margin_top,
+        margin_bottom=text_margin_bottom,
+        margin_left=text_margin_left or title_margin_left,
+        margin_right=text_margin_right or title_margin_right,
         line_width=line_width,
+        align=text_align,
     )
     return f"{title_section}\n{body_section}"
 
@@ -78,15 +88,20 @@ def block(
     text_styles: int | str | list[int | str] = None,
     text_color: int | str | tuple = None,
     bg_color: int | str | tuple = None,
-    char_top: Stringable | None = None,
-    char_bottom: Stringable | None = None,
-    char_left: Stringable | None = None,
-    char_right: Stringable | None = None,
+    char_top: Stringable | None = "━",
+    char_bottom: Stringable | None = "━",
+    char_left: Stringable | None = "┃",
+    char_right: Stringable | None = "┃",
+    char_top_left: Stringable | None = "┏",
+    char_top_right: Stringable | None = "┓",
+    char_bottom_left: Stringable | None = "┗",
+    char_bottom_right: Stringable | None = "┛",
     margin_top: int | None = None,
     margin_bottom: int | None = None,
     margin_left: int | None = None,
     margin_right: int | None = None,
     line_width: int = 50,
+    align: Literal["left", "right", "center"] = "left",
 ):
     lines = _textwrap.wrap(
         text=str(text),
@@ -110,7 +125,16 @@ def block(
         lines = lines + [" " * line_width] * margin_bottom
     formatted_lines = []
     for line in lines:
-        line = line.ljust(line_width)
+        screen_width = _wcwidth.wcswidth(line)
+        char_width = len(line)
+        extra_padding = char_width - screen_width
+        width = line_width + extra_padding
+        if align == "left":
+            line = line.ljust(width)
+        elif align == "right":
+            line = line.rjust(width)
+        else:
+            line = line.center(width)
         formatted_lines.append(
             inline(
                 line,
@@ -127,18 +151,12 @@ def block(
         char_top = char_bottom or ""
     if char_bottom is None:
         char_bottom = char_top or ""
-    if char_left is None:
-        char_left = char_right or ""
-    if char_right is None:
-        char_right = char_left or ""
     total_line_width = line_width + (margin_left or 0) + (margin_right or 0)
-    space_left = " " * len(str(char_left)) if char_left else 0
-    space_right = " " * len(str(char_right)) if char_right else 0
     if char_top:
-        line_top = f"{space_left}{char_top * total_line_width}{space_right}"
+        line_top = f"{char_top_left}{char_top * total_line_width}{char_top_right}"
         formatted_lines.insert(0, line_top)
     if char_bottom:
-        line_bottom = f"{space_left}{char_bottom * total_line_width}{space_right}"
+        line_bottom = f"{char_bottom_left}{char_bottom * total_line_width}{char_bottom_right}"
         formatted_lines.append(line_bottom)
     return "\n".join(formatted_lines)
 
